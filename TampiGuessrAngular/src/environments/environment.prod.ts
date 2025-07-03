4f5f21610b42ffe017e9c829de48cc4dc1278432
Yael <142482823+Y4ELX@users.noreply.github.com>
2025-07-03 00:28:44 -0600

    feat: initialize TampiGuessrAngular project with Angular 12 setup
    - Added package.json with Angular dependencies and scripts.
    - Created app-routing.module.ts for routing configuration.
    - Implemented app.component.ts with basic structure and title.
    - Developed app.component.html with game interface and buttons.
    - Added app.component.spec.ts for unit testing of AppComponent.
    - Set up app.module.ts to declare and bootstrap AppComponent.
    - Included environment files for production and development configurations.
    - Added favicon.ico and index.html for application entry point.
    - Configured main.ts for bootstrapping the Angular application.
    - Included polyfills.ts for browser compatibility.
    - Created styles.css for global styles.
    - Set up test.ts for Karma testing configuration.
    - Configured TypeScript settings in tsconfig files.

---
+++ TampiGuessrAngular/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC3SS1QKSivXg3JxFX9BPzWpZQhNEMUgGE",
+    authDomain: "tampiguessr-17a8b.firebaseapp.com",
+    projectId: "tampiguessr-17a8b",
+    storageBucket: "tampiguessr-17a8b.firebasestorage.app",
+    messagingSenderId: "55666345891",
+    appId: "1:55666345891:web:315c3bfec3b9fe89f67c16"
+  },
+};
